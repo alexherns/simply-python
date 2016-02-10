a5b0f85f1e7e7d77f75b267a7a038c6d8e78bd74
alexherns <alexherns@gmail.com>
2016-02-09 20:33:07 -0800

    Added Miller-Rabin testing

--- tools.py
+++ tools.py
@@ -5,6 +5,35 @@ def simple_hash(s, base):
         cumsum+= base**(len(s)-i) * ord(s[i])
     return cumsum
 
+prime_tests= [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+prime_bases= [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 325, 9375, 28178,
+        450775, 9780504, 1795265022]
+
+def _test_composite(a, d, n, s):
+    if pow(a, d, n) == 1:
+        return False
+    for i in range(s):
+        if pow(a, 2**i * d, n) == n-1:
+            return False
+    return True
+
+def is_prime(n):
+    """Return n is prime.
+    Simple implementation of Miller-Rabin primality test inspired by Rosetta
+    Code"""
+    if n in (0, 1, 2):
+        return True
+    if any((n % p) == 0 for p in prime_tests):
+        return False
+    d, s= n-1, 0
+    while not d << 2:
+        d>>= 1
+        s+= 1
+    if n < 3474749660383:
+        return not any(_test_composite(a, d, n, s) for a in (2, 3, 5, 7, 11,
+            13, 17))
+    return not any(_test_composite(a, d, n, s) for a in prime_tests)
+
 import cmath
 def DFT(x):
     """Computes the discrete Fourier transform of an input sampling"""
